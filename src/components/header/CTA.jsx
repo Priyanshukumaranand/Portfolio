631df8de8bca0f1dfa47f10b6e57a9c91f89c17c
Priyanshu Kumar Anand <118902541+Priyanshukumaranand@users.noreply.github.com>
2024-08-07 00:22:16 +0530

    small fix

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,6 +3,7 @@ import CV from '../../assets/resume.pdf'
 
 const CTA = () => {
   return (
+    
       <div className="cta">
           <a href={CV} download className='btn'>Download CV</a>
           <a href="#contact" className='btn btn-primary'>Let's Talk</a>
