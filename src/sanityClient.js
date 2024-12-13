d0b80d2f764fc51db3d9cd12557db6499b65cc75
Priyanshu Kumar Anand <118902541+Priyanshukumaranand@users.noreply.github.com>
2024-12-13 15:14:38 +0530

    profile pic and resume are fetched from sanity api

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,7 +1,7 @@
 // filepath: src/sanityClient.js
-import sanityClient from '@sanity/client';
+import createClient from '@sanity/client';
 
-export default sanityClient({
+export default createClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID, // Your project ID from env
   dataset: 'production', // Your dataset name
   useCdn: true, // `false` if you want to ensure fresh data
